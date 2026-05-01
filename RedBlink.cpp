e55c577d93d10925f49c931b4d122864fb810ea7
Darlene-13 <darlenewendie@gmail.com>
2026-05-01 11:41:40 +0300

    red LED blink project

---
+++ RedBlink.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 12
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);  // LED ON
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);   // LED OFF
+  delay(1000);
+}
